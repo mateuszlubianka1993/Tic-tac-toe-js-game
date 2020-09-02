57502f5ff4f142d8bc3ccb10b6d4482f2b8cdb2b
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-09-02 11:58:52 +0200

    Extract functions to another file.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
+const playAgain = require('./util').playAgain;
+const findWinner = require('./util').findWinner;
+
 document.addEventListener('DOMContentLoaded', function() {
 
+    const $player = document.getElementById('player');
     const $boxes = document.querySelectorAll('#big-box div');
+    let isWinner = false;
     let ticTac = 0;
 
     for (let i = 0; i < $boxes.length; i++) {
@@ -10,13 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
                     this.innerHTML = 'X';
                     $player.innerHTML = 'O Player Turn';
-                    findWinner();
+                    findWinner(isWinner);
                     ticTac += 1;
                 } else {
 
                     this.innerHTML = 'O';
                     $player.innerHTML = 'X Player Turn';
-                    findWinner();
+                    findWinner(isWinner);
                     ticTac += 1;
                 }
             }
@@ -26,63 +31,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Play Again function
     const $button = document.getElementById('play-again');
     $button.addEventListener('click', function() {
-        for (let i = 0; i < $boxes.length; i++) {
-            $boxes[i].innerHTML = '';
-            $boxes[i].classList.remove('win-box');
-        }
-        $player.innerHTML = 'PLAY';
-        isWinner = false;
+        playAgain($boxes, $player, isWinner);
     });
-
 });
-
-const $player = document.getElementById('player');
-let isWinner = false;
-
-const findWinner = () => {
-
-    const $box1 = document.getElementById('box1');
-    const $box2 = document.getElementById('box2');
-    const $box3 = document.getElementById('box3');
-    const $box4 = document.getElementById('box4');
-    const $box5 = document.getElementById('box5');
-    const $box6 = document.getElementById('box6');
-    const $box7 = document.getElementById('box7');
-    const $box8 = document.getElementById('box8');
-    const $box9 = document.getElementById('box9');
-
-
-    if ($box1.innerHTML !== '' && $box1.innerHTML === $box2.innerHTML && $box1.innerHTML === $box3.innerHTML) {
-        changeColor($box1, $box2, $box3);
-    }
-    if ($box4.innerHTML !== '' && $box4.innerHTML === $box5.innerHTML && $box4.innerHTML === $box6.innerHTML) {
-        changeColor($box4, $box5, $box6);
-    }
-    if ($box7.innerHTML !== '' && $box7.innerHTML === $box8.innerHTML && $box7.innerHTML === $box9.innerHTML) {
-        changeColor($box7, $box8, $box9);
-    }
-    if ($box1.innerHTML !== '' && $box1.innerHTML === $box4.innerHTML && $box1.innerHTML === $box7.innerHTML) {
-        changeColor($box1, $box4, $box7);
-    }
-    if ($box2.innerHTML !== '' && box2.innerHTML === $box5.innerHTML && $box2.innerHTML === $box8.innerHTML) {
-        changeColor($box2, $box5, $box8);
-    }
-    if ($box3.innerHTML !== '' && $box3.innerHTML === $box6.innerHTML && $box3.innerHTML === $box9.innerHTML) {
-        changeColor($box3, $box6, $box9);
-    }
-    if ($box1.innerHTML !== '' && $box1.innerHTML === $box5.innerHTML && $box1.innerHTML === $box9.innerHTML) {
-        changeColor($box1, $box5, $box9);
-    }
-    if ($box3.innerHTML !== '' && $box3.innerHTML === $box5.innerHTML && $box3.innerHTML === $box7.innerHTML) {
-        changeColor($box3, $box5, $box7);
-    }
-
-};
-
-const changeColor = (box1, box2, box3) => {
-    box1.classList.add('win-box');
-    box2.classList.add('win-box');
-    box3.classList.add('win-box');
-    $player.innerHTML = box1.innerHTML + ' player won!';
-    isWinner = true;
-};
---
+++ src/js/util.js
@@ -0,0 +1,61 @@
+const playAgain = ($boxes, $player, isWinner) => {
+
+    for (let i = 0; i < $boxes.length; i++) {
+        $boxes[i].innerHTML = '';
+        $boxes[i].classList.remove('win-box');
+    }
+    $player.innerHTML = 'PLAY';
+    isWinner = false;
+}
+
+const changeColor = (box1, box2, box3, isWinner) => {
+    const $player = document.getElementById('player');
+
+    box1.classList.add('win-box');
+    box2.classList.add('win-box');
+    box3.classList.add('win-box');
+    $player.innerHTML = box1.innerHTML + ' player won!';
+    isWinner = true;
+};
+
+const findWinner = (isWinner) => {
+
+    const $box1 = document.getElementById('box1');
+    const $box2 = document.getElementById('box2');
+    const $box3 = document.getElementById('box3');
+    const $box4 = document.getElementById('box4');
+    const $box5 = document.getElementById('box5');
+    const $box6 = document.getElementById('box6');
+    const $box7 = document.getElementById('box7');
+    const $box8 = document.getElementById('box8');
+    const $box9 = document.getElementById('box9');
+
+    if ($box1.innerHTML !== '' && $box1.innerHTML === $box2.innerHTML && $box1.innerHTML === $box3.innerHTML) {
+        changeColor($box1, $box2, $box3, isWinner);
+    }
+    if ($box4.innerHTML !== '' && $box4.innerHTML === $box5.innerHTML && $box4.innerHTML === $box6.innerHTML) {
+        changeColor($box4, $box5, $box6, isWinner);
+    }
+    if ($box7.innerHTML !== '' && $box7.innerHTML === $box8.innerHTML && $box7.innerHTML === $box9.innerHTML) {
+        changeColor($box7, $box8, $box9, isWinner);
+    }
+    if ($box1.innerHTML !== '' && $box1.innerHTML === $box4.innerHTML && $box1.innerHTML === $box7.innerHTML) {
+        changeColor($box1, $box4, $box7, isWinner);
+    }
+    if ($box2.innerHTML !== '' && box2.innerHTML === $box5.innerHTML && $box2.innerHTML === $box8.innerHTML) {
+        changeColor($box2, $box5, $box8, isWinner);
+    }
+    if ($box3.innerHTML !== '' && $box3.innerHTML === $box6.innerHTML && $box3.innerHTML === $box9.innerHTML) {
+        changeColor($box3, $box6, $box9, isWinner);
+    }
+    if ($box1.innerHTML !== '' && $box1.innerHTML === $box5.innerHTML && $box1.innerHTML === $box9.innerHTML) {
+        changeColor($box1, $box5, $box9, isWinner);
+    }
+    if ($box3.innerHTML !== '' && $box3.innerHTML === $box5.innerHTML && $box3.innerHTML === $box7.innerHTML) {
+        changeColor($box3, $box5, $box7, isWinner);
+    }
+};
+
+exports.playAgain = playAgain;
+exports.changeColor = changeColor;
+exports.findWinner = findWinner;
