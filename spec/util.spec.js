751737070d81b8a221af360e76b0d5f3766ee14c
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-09-02 15:59:42 +0200

    Install and config for jasmine DOM testing.

--- spec/util.spec.js
+++ spec/util.spec.js
@@ -1,12 +1,30 @@
+let { JSDOM } = require('jsdom');
+
 const playAgain = require('../src/js/util').playAgain;
 
 describe("A playAgain function", function() {
     let isWinner;
-    const $boxes = [];
-    const $player = {};
+    let browser;
+
+    beforeEach(done => {
+        JSDOM.fromFile('./dist/index.html', {
+
+        }).then(res => {
+            browser = res;
+
+            done();
+        })
+    })
+
+    afterEach(() => {
+        browser.window.close();
+    })
 
     it("should change isWinner value to 'false'.", function() {
         isWinner = true;
+        const $boxes = browser.window.document.querySelectorAll('#big-box div');
+        const $player = browser.window.document.getElementById('player');
+
         isWinner = playAgain($boxes, $player, isWinner);
         expect(isWinner).toBe(false);
     });
