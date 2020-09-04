7a66adf5ba36f47a01f9ba9cc83831745ee84be0
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-09-04 12:56:02 +0200

    Add unit tests for playAgain function.

--- spec/util.spec.js
+++ spec/util.spec.js
@@ -28,4 +28,29 @@ describe("A playAgain function", function() {
         isWinner = playAgain($boxes, $player, isWinner);
         expect(isWinner).toBe(false);
     });
+
+    it("should change #player text to 'PLAY'.", function() {
+        isWinner = true;
+        const $boxes = browser.window.document.querySelectorAll('#big-box div');
+        const $player = browser.window.document.getElementById('player');
+
+        isWinner = playAgain($boxes, $player, isWinner);
+        const result = $player.innerHTML;
+        expect(result).toBe('PLAY');
+    });
+
+    it("should change every .small-box text to ''.", function() {
+        isWinner = true;
+        const $boxes = browser.window.document.querySelectorAll('#big-box div');
+        const $player = browser.window.document.getElementById('player');
+
+        isWinner = playAgain($boxes, $player, isWinner);
+        const result = [];
+        for (let i = 0; i < $boxes.length; i++) {
+            if ($boxes[i].innerHTML == '') {
+                result.push($boxes[i]);
+            }
+        }
+        expect(result.length).toBe($boxes.length);
+    });
 });
